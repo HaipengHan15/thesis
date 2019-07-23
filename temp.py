f92f31e201399cfc3f2cae8285479fe125e71b7b
HaipengHan15 <34022748+HaipengHan15@users.noreply.github.com>
2019-07-23 14:49:01 +0800

    utility

--- temp.py
+++ temp.py
@@ -9,11 +9,17 @@ font1 = {'family': 'Times New Roman',
          'size': 23,
          }
 
-NM = np.arange(1, 11)
+'''NM = np.arange(1, 11)
 profit = [0.4899322175, 0.39817792453875966, 0.469256256094979, 0.5131004117650224, 0.5695896279880446,
           0.5877135899733421, 0.6168219934949997, 0.6278313269791992, 0.6445080995095251, 0.6519128975388344]
 result = np.array(profit)
 plt.plot(NM, result)  # 双方都歧视定价时的利润
 plt.xlabel('NM', fontsize=12)
 plt.ylabel(u'利润', fontsize=12)
-pylab.show()
+pylab.show()'''
+if 1 < 2:
+    print('aaa')
+elif 1 < 3:
+    print('bbb')
+else:
+    print('ccc')
